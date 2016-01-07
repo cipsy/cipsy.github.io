10984cd2a72c45d0bf7b441cb36935806e0e7c7e
Ciprian <ciprian.medar@yahoo.com>
2016-01-07 20:31:48 +0200

    bringing jekyll on the stage

---
+++ js/contact_map.js
@@ -0,0 +1,17 @@
+jQuery(function ($) {
+        function init_map1() {
+            var myLocation = new google.maps.LatLng(44.398099, 26.067037);
+            var mapOptions = {
+                center: myLocation,
+                zoom: 16
+            };
+            var marker = new google.maps.Marker({
+                position: myLocation,
+                title: "Property Location"
+            });
+            var map = new google.maps.Map(document.getElementById("map1"),
+                mapOptions);
+            marker.setMap(map);
+        }
+        init_map1();
+    });
